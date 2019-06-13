888695737b7195e65fef0055b9e7e8a6f03c6b67
AQ <aq@okaq.com>
2019-06-13 09:00:16 -0400

    Server cache

--- aomi.go
+++ aomi.go
@@ -13,9 +13,32 @@ const (
 	INDEX = "aomi.html"
 )
 
+var (
+	// global cache, accessed via channel
+	C map[string]string
+	W chan w
+)
+
+type w struct {
+	key string
+	val string
+	r chan bool
+}
+
 func motd() {
 	fmt.Println("running on localhost:8080...")
 	fmt.Printf("%s\n", time.Now().String())
+}
+
+func cache() {
+	C = make(map[string]string)
+	W = make(chan w)
+	go func() {
+		// recievers
+		w0 := <-W
+		C[w0.key] = w0.val
+		w0.r <- true
+	}()
 }
 
 func AomiHandler(w http.ResponseWriter, r *http.Request) {
@@ -23,9 +46,17 @@ func AomiHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, INDEX)
 }
 
+func DataHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
+	// parse json {key:val} data
+	// send to channel for global state
+}
+
 func main() {
 	motd()
+	cache()
 	http.HandleFunc("/", AomiHandler)
+	http.HandleFunc("/a", DataHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
