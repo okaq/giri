d7b9685cfbb1bbee25e49b49fe0f7bc1c6044893
AQ <aq@okaq.com>
2019-07-12 19:55:59 -0400

    Cache init

--- domi.go
+++ domi.go
@@ -13,6 +13,11 @@ const (
 	INDEX = "domi.html"
 )
 
+
+var (
+	c map[string]string
+)
+
 func motd() {
 	fmt.Println(time.Now().String())
 	fmt.Println("web serve localhost:8080")
@@ -23,9 +28,19 @@ func DomiHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w,r,INDEX)
 }
 
+func CacheHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
+	// print to output stream
+}
+
+func cache() {
+	c = make(map[string]string)
+}
+
 func main() {
 	motd()
 	// server side data cache init
+	cache()
 	http.HandleFunc("/", DomiHandler)
 	http.ListenAndServe(":8080", nil)
 }
