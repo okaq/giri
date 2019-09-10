078141c0c4c06f1a8fd6636063eea51489268d54
AQ <aq@okaq.com>
2019-09-10 10:51:44 -0400

    DOM init

--- hiki.go
+++ hiki.go
@@ -17,6 +17,13 @@ func motd() {
 	fmt.Println(time.Now().String())
 }
 
+func cache() {
+	// init memory store
+	// map[key]value strings
+	// generate stats for user metadata
+	// visualize peer network
+}
+
 func HikiHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r)
 	http.ServeFile(w,r,INDEX)
@@ -24,6 +31,7 @@ func HikiHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	motd()
+	cache()
 	http.HandleFunc("/", HikiHandler)
 	http.ListenAndServe(":8080", nil)
 }
