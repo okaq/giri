f6cacdc915911727e7c548bc65922cf0450fbc05
AQ <aq@okaq.com>
2019-10-25 11:02:49 -0400

    Key value string serializer

--- lito/lito.go
+++ lito/lito.go
@@ -10,6 +10,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -66,6 +67,26 @@ func insp() {
 	fmt.Println(b1)
 	fmt.Println(len(b1))
 	fmt.Println(string(b1))
+	// empty!
+
+	// just try a map
+	f2 := make(map[string]string)
+	for i, f0 := range f {
+		// s0 := string(i)
+		s0 := strconv.Itoa(i)
+		i1 := strconv.FormatInt(f0.Size(), 10)
+		s1 := fmt.Sprintf("%s|%s|%s", f0.Name(), i1, f0.ModTime().String())
+		f2[s0] = s1
+	}
+	fmt.Println(f2)
+	b2, err := json.Marshal(f2)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(b2)
+	fmt.Println(len(b2))
+	fmt.Println(string(b2))
+
 }
 
 func LitoHandler(w http.ResponseWriter, r *http.Request) {
